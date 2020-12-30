e99691b7d820910a250997da53c4e12e02c746fd
Alexander Kirshin <alexander.kirshin.dev@gmail.com>
2020-12-30 20:37:54 +0700

    initial commit

---
+++ src/CheckNalogPd.php
@@ -0,0 +1,64 @@
+<?php
+
+
+namespace Kirshin\CheckNalog;
+
+
+use Exception;
+use JsonException;
+
+class CheckNalogPd
+{
+    private $fields;
+
+    private const URL = 'https://statusnpd.nalog.ru/api/v1/tracker/taxpayer_status';
+
+
+    /**
+     * CheckNalogPd constructor.
+     * @param string $inn
+     * @param string|null $date
+     * @throws JsonException
+     */
+    public function __construct(string $inn, string $date = null) {
+        $this->setFields($inn, $date);
+    }
+
+    public function check(): void
+    {
+        $response = $this->httpPost(self::URL, $this->fields);
+        // :TODO
+    }
+
+    /**
+     * @param $url
+     * @param $data
+     * @return bool|string
+     */
+    private function httpPost($url, $data) {
+        $curl = curl_init($url);
+        curl_setopt($curl, CURLOPT_POST, true);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, [
+            'Content-Type:application/json',
+            'Content-Length: ' . strlen($data)
+        ]);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        $response = curl_exec($curl);
+        curl_close($curl);
+        return $response;
+    }
+
+    /**
+     * @param $inn
+     * @param $date
+     * @throws JsonException
+     */
+    private function setFields($inn, $date): void
+    {
+        $this->fields = json_encode([
+            'inn' => $inn,
+            'requestDate' => $date ?? date("Y-m-d", strtotime("yesterday"))
+        ], JSON_THROW_ON_ERROR);
+    }
+}
